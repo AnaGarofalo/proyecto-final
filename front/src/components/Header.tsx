b434a3eaa668793b4a386c4448beaf527ff68c45
Clara <franzoniclara@gmail.com>
2025-10-14 18:07:29 -0300

    V6 - C 01 - Mejora en la funcion de extraer email del usuario para motrar en el header

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -3,20 +3,8 @@ import { useState, useEffect } from "react";
 import nestleLogo from "../assets/logo/nestleLogo.png";
 import { Colors } from "../utils/Colors";
 import { DRAWER_WIDTH_PX, HEADER_HEIGHT_PX } from "./Layout";
+import { getUserEmailFromToken } from "../utils/JwtUtils"; 
 
-// Función para extraer el email del usuario desde el token JWT
-const getUserEmailFromToken = (): string | null => {
-  try {
-    const token = localStorage.getItem('access_token');
-    if (!token) return null;
-    
-    const payload = JSON.parse(atob(token.split('.')[1]));
-    return payload.sub; // 'sub' contiene el email en el JWT
-  } catch (error) {
-    console.error('Error al decodificar el token:', error);
-    return null;
-  }
-};
 
 const Header = () => {
   const [userEmail, setUserEmail] = useState<string | null>(null);
