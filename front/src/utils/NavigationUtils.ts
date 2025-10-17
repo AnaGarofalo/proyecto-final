108816b3f195d599725191b049e9958f72151e8e
Christian <b.christian.fuentes@gmail.com>
2025-10-17 02:33:39 -0300

    se borra la pantalla de confirmacion y se reemplaza por un toast

--- front/src/utils/NavigationUtils.ts
+++ front/src/utils/NavigationUtils.ts
@@ -2,7 +2,6 @@ export const NavigationRoute = {
   LOGIN: "/",
   HOMEPAGE: "/homepage",
   EDIT_PROMPT: "/edit-prompt",
-  EDIT_PROMPT_CONFIRMATION: "/edit-prompt/confirmation",
   DOCUMENTS: "/documents",
   CHAT_USERS: "/chat-users",
   USERS: "/users",
