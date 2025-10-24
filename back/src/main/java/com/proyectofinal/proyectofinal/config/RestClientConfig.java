ee2fa424be433dd3ab8b05c6393b30fe11e13bfe
Christian <b.christian.fuentes@gmail.com>
2025-10-24 01:52:06 -0300

    se cambia de restclient a webclient por rendimiento

--- back/src/main/java/com/proyectofinal/proyectofinal/config/RestClientConfig.java
+++ back/src/main/java/com/proyectofinal/proyectofinal/config/RestClientConfig.java
@@ -3,14 +3,14 @@ package com.proyectofinal.proyectofinal.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
-import org.springframework.web.client.RestClient;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class RestClientConfig {
     @Bean
-    public RestClient whatsappRestClient(WhatsappProperties p) {
+    public WebClient whatsappWebClient(WhatsappProperties p) {
         String base = p.getGraph().getBaseUrl() + "/" + p.getGraph().getVersion();
-        return RestClient.builder()
+        return WebClient.builder()
                 .baseUrl(base)
                 .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + p.getAccessToken())
                 .build();
